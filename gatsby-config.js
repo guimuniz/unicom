3bd65e2c299c6738f27fb3ec0ad06ee34e0abf01
Guilherme Muniz de Castro <guisp1901@gmail.com>
2020-05-09 16:41:56 -0300

    configure styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       resolve: `gatsby-plugin-page-creator`,
       options: {
         path: `${__dirname}/src/pages`,
-        ignore: [`**/styles.js`]
+        ignore: [`**/styles.js`,`**/styles.ts`]
       },
     },
     `gatsby-plugin-sharp`,
